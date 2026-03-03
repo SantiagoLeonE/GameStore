4115d4bf6e9f4c96d40cfc7755046759fccdb7c6
SantiagoLeonE <santiago.leone@uqvirtual.edu.co>
2026-03-03 01:12:42 -0500

    feat: Se implementaron los repository tanto de Desarrolladora como de VideoJuego

--- src/main/java/co/edu/uniquindio/gamestore/repository/DesarrolladoraRepository.java
+++ src/main/java/co/edu/uniquindio/gamestore/repository/DesarrolladoraRepository.java
@@ -1,4 +1,9 @@
 package co.edu.uniquindio.gamestore.repository;
 
-public interface DesarrolladoraRepository {
+import co.edu.uniquindio.gamestore.entity.Desarrolladora;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+//El Jpa nos permite utilizar métodos como save, findById, findAll, entre otros, sin tener que implementar ninguna lógica, el mismo Jpa los intuye
+public interface DesarrolladoraRepository extends JpaRepository<Desarrolladora, Long> {
+
 }
--- src/main/java/co/edu/uniquindio/gamestore/repository/VideoJuegoRepository.java
+++ src/main/java/co/edu/uniquindio/gamestore/repository/VideoJuegoRepository.java
@@ -1,4 +1,28 @@
 package co.edu.uniquindio.gamestore.repository;
 
-public interface VideoJuegoRepository {
+import co.edu.uniquindio.gamestore.entity.VideoJuego;
+import co.edu.uniquindio.gamestore.enums.Genero;
+
+import org.springframework.data.jpa.repository.NativeQuery;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface VideoJuegoRepository extends JpaRepository<VideoJuego,Long> {
+
+    //Se aplica el Derived Query. Buscar un videojuego por su género
+    List<VideoJuego> findByGenero(Genero genero);
+
+    //Se aplica el Derived Query. Buscar un videojuego por su género, en donde se ignoran las mayúsculas y las minúsculas
+    List<VideoJuego> findByGeneroIgnoreCase(Genero generoIgnoreCase);
+
+    //Consulta JPQL: Se buscan videojuegos que están dentro de un rango de precios
+    @Query("SELECT v FROM VideoJuego v WHERE v.precio BETWEEN :min and :max")
+    List<VideoJuego> buscarPorRangoDePrecio(@Param("min") Double min, @Param("max") Double max);
+
+    //Consulta Nativa: Obtener los últimos 5 videojuegos registrados ordenados por la fechaCreacion de forma descendente
+    @NativeQuery(value = "SELECT * FROM VideoJuego ORDER  BY fecha_creacion DESC LIMIT 5")
+    List<VideoJuego> obtenerUltimos5();
 }
